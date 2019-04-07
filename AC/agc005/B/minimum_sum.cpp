8f023bd7298dd1c84b22a4f29f0d4eeb584f2bf2
kHigasa <kojihigasa@gmail.com>
2019-04-07 23:42:46 +0900

    Sun Apr  7 23:42:46 JST 2019

--- AC/agc005/B/minimum_sum.cpp
+++ AC/agc005/B/minimum_sum.cpp
@@ -1,6 +1,30 @@
 #include <cstdio>
+#include <set>
+using namespace std;
+typedef long long ll;
 
 int main() {
+    int N;
+    scanf("%d", &N);
+    int a[N], ra[N + 1];
+    for (int i = 0; i < N; ++i) {
+        scanf("%d", &a[i]);
+        ra[a[i]] = i;
+    }
 
+    ll ans = 0;
+    set<int> s;
+    s.insert(-1); s.insert(N);
+    for (int d = 1; d <= N; ++d) {
+        int i = ra[d];
+        auto it = s.lower_bound(i);
+        int r = *it;
+        it--;
+        int l = *it;
+        ans += ll(d) * (i - l) * (r - i);
+        s.insert(i);
+    }
+
+    printf("%lld\n", ans);
 }
 
