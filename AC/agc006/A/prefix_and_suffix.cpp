f9b57b4de671d7453bbb6c998855167e1f398ee7
khigasa <kojihigasa@gmail.com>
2019-05-29 13:45:27 +0900

    Wed 29 May 2019 01:45:27 PM JST

--- AC/agc006/A/prefix_and_suffix.cpp
+++ AC/agc006/A/prefix_and_suffix.cpp
@@ -1,6 +1,19 @@
-#include <cstdio>
+#include <iostream>
+#include <string>
+using namespace std;
 
 int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    int N;
+    string s, t;
+    cin >> N >> s >> t;
 
+    for (int i = 0; i < N; i++) {
+        if (s.substr(i) == t.substr(0, N - i)) {
+            cout << (s + t.substr(N - i)).size() << "\n";
+            return 0;
+        }
+    }
 }
 
