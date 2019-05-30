512d8124944c1c4711b5ab65762fc454db338204
khigasa <kojihigasa@gmail.com>
2019-05-30 23:47:15 +0900

    Thu 30 May 2019 11:47:15 PM JST

--- AC/agc006/B/median_pyramid_easy.cpp
+++ AC/agc006/B/median_pyramid_easy.cpp
@@ -1,6 +1,13 @@
 #include <cstdio>
+using namespace std;
 
 int main() {
-
+    int N, x;
+    scanf("%d %d", &N, &x);
+    if (x == 1 || x == 2 * N - 1) {
+        printf("No\n");
+    } else {
+        
+    }
 }
 
