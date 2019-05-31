711b998286c5b48ae13ff25ff6d8afafe9310fbe
khigasa <kojihigasa@gmail.com>
2019-05-31 23:44:03 +0900

    Fri 31 May 2019 11:44:02 PM JST

--- AC/agc006/B/median_pyramid_easy.cpp
+++ AC/agc006/B/median_pyramid_easy.cpp
@@ -3,11 +3,23 @@ using namespace std;
 
 int main() {
     int N, x;
+    int ans[20005];
     scanf("%d %d", &N, &x);
     if (x == 1 || x == 2 * N - 1) {
-        printf("No\n");
+        puts("No");
     } else {
-        
+        ans[N - 1] = x;
+        int pos = N - 2;
+        for (int i = 0; i < 2 * N - 2; i++) {
+            if (x <= 1) x = 2 * N;
+            ans[pos] = --x;
+            pos--;
+            if (pos < 0) pos = 2 * N - 2;
+        }
+        puts("Yes");
+        for (int i = 0; i < 2 * N - 1; i++) printf("%d\n", ans[i]);
     }
+
+    return 0;
 }
 
