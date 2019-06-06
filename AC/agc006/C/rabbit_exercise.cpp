6416e6e454fe13241f343bd198333a090a06b1ba
khigasa <kojihigasa@gmail.com>
2019-06-06 23:28:03 +0900

    Thu 06 Jun 2019 11:28:03 PM JST

--- AC/agc006/C/rabbit_exercise.cpp
+++ AC/agc006/C/rabbit_exercise.cpp
@@ -1,7 +1,27 @@
+// ref this submission (https://atcoder.jp/contests/agc006/submissions/954907) .
+// editting....
+
 #include <cstdio>
+#include <algorithm>
+#define SIZE 100005
+
 using namespace std;
+typedef long long ll;
 
 int main() {
-
+    int N;
+    scanf("%d", &N);
+    int x[SIZE];
+    for (int i = 0; i < N; i++) scanf("%d", &x[i]);
+    int X[SIZE];
+    for (int i = 0; i < N; i++) X[i] = i;
+    int M;
+    ll K;
+    scanf("%d %lld", &M, &K);
+    for (int i = 0; i < M; i++) {
+        int a;
+        scanf("%d", &a); a--;
+        swap(X[a - 1], X[a]);
+    }
 }
 
