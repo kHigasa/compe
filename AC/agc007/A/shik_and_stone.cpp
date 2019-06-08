275c9e8fa7d3efa6f8ebff995672e18ebf6fc38b
khigasa <kojihigasa@gmail.com>
2019-06-08 23:40:57 +0900

    Sat 08 Jun 2019 11:40:57 PM JST

--- AC/agc007/A/shik_and_stone.cpp
+++ AC/agc007/A/shik_and_stone.cpp
@@ -1,7 +1,22 @@
 #include <cstdio>
+
 using namespace std;
 
 int main() {
-
+    int H, W;
+    scanf("%d %d", &H, &W);
+    int count = 0;
+    char A[10][10];
+    for (int i = 0; i < H; i++) {
+        for (int j = 0; j < W; j++) {
+            scanf("%c", &A[i][j]);
+            if (A[i][j] == '#') count++;
+            if (count > H + W - 1) {
+                puts("Impossible");
+                return 0;
+            }
+        }
+    }
+    puts("Possible");
 }
 
--- AC/agc007/B/construct_sequences.cpp
+++ AC/agc007/B/construct_sequences.cpp
@@ -1,7 +1,10 @@
 #include <cstdio>
+
 using namespace std;
 
 int main() {
-
+    int N;
+    scanf("%d", &N);
+    int p[20005];
 }
 
