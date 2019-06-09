3d30e172d4977c10079ca95fe1d240d79f7f9bee
khigasa <kojihigasa@gmail.com>
2019-06-09 23:52:20 +0900

    Sun 09 Jun 2019 11:52:20 PM JST

--- AC/agc007/B/construct_sequences.cpp
+++ AC/agc007/B/construct_sequences.cpp
@@ -3,8 +3,20 @@
 using namespace std;
 
 int main() {
-    int N;
-    scanf("%d", &N);
-    int p[20005];
+    int n;
+    scanf("%d", &n);
+    int P[20005];
+    int A[20005], B[20005];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &P[i]);
+        A[i] = (i + 1) * n;
+        B[i] = (n - i) * n;
+    }
+    for (int i = 0; i < n; i++) A[P[i]] += i;
+    for (int i = 0; i < n; i++) printf("%d ", A[i]);
+    printf("\n");
+    for (int i = 0; i < n; i++) printf("%d ", B[i]);
+    printf("\n");
+    return 0;
 }
 
