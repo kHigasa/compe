7bfb8112c5aa211968fad223016978640fac5508
khigasa <kojihigasa@gmail.com>
2019-06-14 23:39:51 +0900

    Fri 14 Jun 2019 11:39:51 PM JST

--- AC/agc010/A/addition.cpp
+++ AC/agc010/A/addition.cpp
@@ -4,5 +4,14 @@ using namespace std;
 typedef long long ll;
 
 int main() {
-
+    int n;
+    scanf("%d", &n);
+    ll a, sum = 0;
+    for (int i = 0; i < n; i++) {
+        scanf("%lld", &a);
+        sum += a;
+    }
+    if ((sum & 1) == 0) puts("YES");
+    else puts("NO");
+    return 0;
 }
--- AC/agc010/B/boxes.cpp
+++ AC/agc010/B/boxes.cpp
@@ -4,5 +4,30 @@ using namespace std;
 typedef long long ll;
 
 int main() {
-
+    int n;
+    scanf("%d", &n);
+    ll pat_sum = n * (n + 1) / 2;
+    int A[100005];
+    ll sum = 0;
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &A[i]);
+        sum += A[i];
+    }
+    if (sum % pat_sum != 0) {
+        puts("NO");
+        return 0;
+    }
+    else {
+        int iter = sum / pat_sum;
+        int dif;
+        for (int i = 0; i < n; i++) {
+            dif = A[(i + 1) % n] - A[i] - iter;
+            if (dif > 0 || (-dif % n) != 0) {
+                puts("NO");
+                return 0;
+            }
+        }
+        puts("YES");
+        return 0;
+    }
 }
