f6ed2eb51d61beff2d94ef9d5e29d70382668ca5
khigasa <kojihigasa@gmail.com>
2019-06-16 23:14:33 +0900

    Sun 16 Jun 2019 11:14:33 PM JST

--- AC/agc011/A/airport_bus.cpp
+++ AC/agc011/A/airport_bus.cpp
@@ -1,8 +1,20 @@
 #include <cstdio>
 
 using namespace std;
-typedef long long ll;
 
 int main() {
-
+    int n, c, k;
+    scanf("%d %d %d", &n, &c, &k);
+    int T[100005];
+    for (int i = 0; i < n; i++) scanf("%d", &T[i]);
+    int ans = 0;
+    for (int i = 0, j = 0; i < n; i++) {
+        if (T[i] > T[j] + k || (i - j) == c) {
+            ans++;
+            j = i;
+        }
+    }
+    ans++;
+    printf("%d\n", ans);
+    return 0;
 }
