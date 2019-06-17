2d3a93c6eb06e7afc33f5af3e1b9be2c8ceb60e0
khigasa <kojihigasa@gmail.com>
2019-06-17 23:37:13 +0900

    Mon 17 Jun 2019 11:37:13 PM JST

--- AC/agc012/A/atcoder_group_contest.cpp
+++ AC/agc012/A/atcoder_group_contest.cpp
@@ -1,8 +1,17 @@
 #include <cstdio>
+#include <algorithm>
 
 using namespace std;
 typedef long long ll;
 
 int main() {
-
+    int n;
+    scanf("%d", &n);
+    int A[300005];
+    for (int i = 0; i < 3 * n; i++) scanf("%d", &A[i]);
+    sort(A, A + 3 * n);
+    ll ans = 0;
+    for (int i = n; i < 3 * n; i += 2) ans += 1ll * A[i];
+    printf("%lld\n", ans);
+    return 0;
 }
