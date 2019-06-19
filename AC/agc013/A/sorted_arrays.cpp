5581fbaae1ca14183df85ec827459130ed643de7
khigasa <kojihigasa@gmail.com>
2019-06-19 23:54:14 +0900

    Wed 19 Jun 2019 11:54:14 PM JST

--- AC/agc013/A/sorted_arrays.cpp
+++ AC/agc013/A/sorted_arrays.cpp
@@ -1,8 +1,23 @@
 #include <cstdio>
 
 using namespace std;
-typedef long long ll;
 
 int main() {
-
+    int n;
+    scanf("%d", &n);
+    int A[100005];
+    for (int i = 0; i < n; i++) scanf("%d", &A[i]);
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        ans++;
+        while (i + 1 < n && A[i] == A[i + 1]) i++;
+        if (i == n - 1) break;
+        if (A[i] < A[i + 1]) {
+            while (i + 1 < n && A[i] <= A[i + 1]) i++
+        } else {
+            while (i + 1 < n && A[i] >= A[i + 1]) i++
+        }
+    }
+    printf("%d\n", ans);
+    return 0;
 }
