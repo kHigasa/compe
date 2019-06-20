dc68583822723dd5b253b422f5b12d9406d6c42e
khigasa <kojihigasa@gmail.com>
2019-06-20 23:57:45 +0900

    Thu 20 Jun 2019 11:57:44 PM JST

--- AC/agc013/B/hamiltonish_path.cpp
+++ AC/agc013/B/hamiltonish_path.cpp
@@ -1,8 +1,39 @@
 #include <cstdio>
+#include <algorithm>
+#include <vector>
+#define SIZE 100005
 
 using namespace std;
-typedef long long ll;
+
+int n, m, sz, R[SIZE];
+bool V[SIZE];
+vector<int> gph[SIZE];
+
+void dfs(int x) {
+    if (!V[x]) R[sz++] = x;
+    V[x] = 1;
+    for (int i : gph[x]) {
+        if (!V[i]) {
+            dfs(i);
+            break;
+        }
+    }
+}
 
 int main() {
-
+    scanf("%d %d", &n, &m);
+    int a, b;
+    for (int i = 0; i < m; i++) {
+        scanf("%d %d", &a, &b);
+        a--; b--;
+        gph[a].push_back(b);
+        gph[b].push_back(a);
+    }
+    dfs(0);
+    reverse(R, R + sz);
+    dfs(0);
+    printf("%d\n", sz);
+    for (int i = 0; i < sz; i++) printf("%d ", R[i] + 1);
+    printf("\n");
+    return 0;
 }
