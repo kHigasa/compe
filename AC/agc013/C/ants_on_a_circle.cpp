230578e2dcf09dd32594ed52337b583570c5a48b
khigasa <kojihigasa@gmail.com>
2019-06-22 23:54:07 +0900

    Sat 22 Jun 2019 11:54:06 PM JST

--- AC/agc013/C/ants_on_a_circle.cpp
+++ AC/agc013/C/ants_on_a_circle.cpp
@@ -1,8 +1,35 @@
 #include <cstdio>
+#include <algorithm>
+#include <utility>
+#include <vector>
 
 using namespace std;
 typedef long long ll;
 
 int main() {
-
+    int n, l, t;
+    scanf("%d %d %d", &n, &l, &t);
+    int X[100005], W[100005];
+    for (int i = 0; i < n; i++) {
+        scanf("%d %d", &X[i], &W[i]);
+        if (W[i] == 2) W[i] = -1;
+    }
+    pair<int, int> pr[100005];
+    for (int i = 0; i < n; i++) pr[i] = make_pair(X[i], i);
+    sort(pr, pr + n);
+    vector<ll> vec;
+    ll nc = 0;
+    for (int i = 0; i < n; i++) {
+        ll pos = X[i] + W[i] * t;
+        ll fpos = ((pos % l) + l) % l;
+        ll g = pos > 0 ? pos / l : (pos - l + 1) / l;
+        nc += g;
+        vec.push_back(fpos);
+    }
+    nc = ((nc % n) + n) % n;
+    sort(vec.begin(), vec.end());
+    int ans[100005];
+    for (int i = 0; i < n; i++) ans[pr[i].second] = vec[(i + nc) % n];
+    for (int i = 0; i < n; i++) printf("%d\n", ans[i]);
+    return 0;
 }
