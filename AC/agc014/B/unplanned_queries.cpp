8b91988dac3737d6530673819d901669dfda5d58
khigasa <kojihigasa@gmail.com>
2019-06-24 23:00:27 +0900

    Mon 24 Jun 2019 11:00:26 PM JST

--- AC/agc014/B/unplanned_queries.cpp
+++ AC/agc014/B/unplanned_queries.cpp
@@ -1,8 +1,24 @@
 #include <cstdio>
+#define rep(i, n) for (int i = 0; (i) < (int)(n); (i)++)
 
 using namespace std;
-typedef long long ll;
 
 int main() {
-
+    int n, m;
+    scanf("%d %d", &n, &m);
+    int a, b;
+    int C[100005];
+    rep(i, m) {
+        scanf("%d %d", &a, &b);
+        a--; b--;
+        C[a]++; C[b]++;
+    }
+    rep(i, n) {
+        if ((C[i] & 1) != 0) {
+            puts("NO");
+            return 0;
+        }
+    }
+    puts("YES");
+    return 0;
 }
